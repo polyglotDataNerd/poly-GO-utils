4f825bd08839c41df6ab435458c1cd5360257951
polyglotDataNerd <gerardnino@gmail.com>
2020-03-16 10:00:42 -0700

    added log file

--- utils/Logger.go
+++ utils/Logger.go
@@ -19,31 +19,34 @@ func Init(traceHandle io.Writer,
 	infoHandle io.Writer,
 	warningHandle io.Writer,
 	errorHandle io.Writer) {
+
+	logpath := "/var/tmp/utils.log"
+	file, err := os.OpenFile(logpath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	defer file.Close()
+	if err != nil {
+		Error.Print(err)
+	}
 	flag.Parse()
 	Trace = log.New(traceHandle,
 		"TRACE: ",
 		log.Ldate|log.Ltime|log.LstdFlags|log.Lshortfile)
+	Trace.SetOutput(file)
 
 	Info = log.New(infoHandle,
 		"INFO: ",
 		log.Ldate|log.Ltime|log.LstdFlags|log.Lshortfile)
+	Info.SetOutput(file)
 
 	Warning = log.New(warningHandle,
 		"WARNING: ",
 		log.Ldate|log.Ltime|log.LstdFlags|log.Lshortfile)
+	Warning.SetOutput(file)
 
 	Error = log.New(errorHandle,
 		"ERROR: ",
 		log.Ldate|log.Ltime|log.LstdFlags|log.Lshortfile)
+	Error.SetOutput(file)
 }
 func init() {
 	Init(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
-	logpath := "/var/tmp/utils.log"
-	flag.Parse()
-	file, err := os.Create(logpath)
-	if err != nil {
-		Error.Print(err)
-	}
-	Log := log.New(file, "", log.Ldate|log.Ltime|log.LstdFlags|log.Lshortfile)
-	Log.Println("LogFile : " + logpath)
 }
