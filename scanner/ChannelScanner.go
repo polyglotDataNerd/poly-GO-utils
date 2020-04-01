4c1b361535b7b026ea90066ca5155993371ce4e1
polyglotDataNerd <gerardnino@gmail.com>
2020-04-01 07:15:07 -0700

    added flat object to channel scanner

--- scanner/ChannelScanner.go
+++ scanner/ChannelScanner.go
@@ -29,24 +29,40 @@ func ProcessObj(line chan string, bucket string, key string) {
 	fmt.Println("sent all lines")
 }
 
-func ProcessDir(line chan string, bucket string, key string) {
-	var wg sync.WaitGroup
-	defer close(line)
-	/* close on the sender (producer) and NOT the receiver (consumer) */
-	obj, objerr := aws.S3Obj{
-		bucket,
-		key}.S3ReadObjGZIPDir(aws.SessionGenerator("default", "us-west-2"))
-
-	if objerr != nil {
-		utils.Error.Fatalln(objerr.Error())
-	}
-	/*
+/*
 	sync.WaitGroup: when spinning up many go routines without corresponding channels there needs to
 	be a way to block and wait until all go routines are done. That is what the WaitGroup is doing
 	for each go routine we increment a block (ADD) once the go routine is completed the DONE method will
 	decrement the WaitGroup while WAIT() waits for all counters to equal 0 which means all go routines are completed
-	 */
-	for k, v := range obj {
+*/
+
+func ProcessDir(line chan string, bucket string, key string, format string) {
+	var wg sync.WaitGroup
+	var source = make(map[string]string)
+	defer close(line)
+	object := aws.S3Obj{
+		Bucket:bucket,
+		Key:   key,
+	}
+
+	if format == "gzip" {
+		/* close on the sender (producer) and NOT the receiver (consumer) */
+		obj , objerr := object.S3ReadObjGZIPDir(aws.SessionGenerator("default", "us-west-2"))
+		source = obj
+		if objerr != nil {
+			utils.Error.Fatalln(objerr.Error())
+		}
+	}
+	if format == "flat" {
+		/* close on the sender (producer) and NOT the receiver (consumer) */
+		obj , objerr := object.S3ReadObjDir(aws.SessionGenerator("default", "us-west-2"))
+		source = obj
+		if objerr != nil {
+			utils.Error.Fatalln(objerr.Error())
+		}
+	}
+
+	for k, v := range source {
 		utils.Info.Println("key:", k)
 		wg.Add(1)
 		go func() {
